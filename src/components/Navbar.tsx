d7d78cfa9ff4f1a51c7050f00216df60e51b70fb
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-03 21:21:15 -0700

    active links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+  const navlinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+    };
+  };
   return (
     <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
+      <NavLink style={navlinkStyles} to={"/about"}>
+        About
+      </NavLink>
+      <NavLink style={navlinkStyles} to={"/"}>
+        Home
+      </NavLink>
     </nav>
   );
 }
