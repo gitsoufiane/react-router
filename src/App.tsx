b760dcc04021a85a654b07d674933d9dc7e9166a
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-03 21:12:07 -0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/about" element={<About />}></Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </>
   );
 }
 
