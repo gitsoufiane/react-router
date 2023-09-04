85635b9e5e169512ad246c96ca21adc79c15f8c9
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-03 21:52:45 -0700

    no match route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/ OrderSummary";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="about" element={<About />}></Route>
         <Route path="order" element={<OrderSummary />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </>
   );
