ce3898657107473431946cf167bc5e8188a3ae23
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-03 21:04:35 -0700

    configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
